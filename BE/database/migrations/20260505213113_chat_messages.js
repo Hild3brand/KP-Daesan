ed7a40fd8e3664e975fa947c22092a55678aeea4
nicholas.hanafi <sysdev.engineer3@idbm.co.id>
2026-05-29 14:16:39 +0700

    update: remove unnecessary item due to conflicted

--- BE/database/migrations/20260505213113_chat_messages.js
+++ BE/database/migrations/20260505213113_chat_messages.js
@@ -1,13 +1,6 @@
 export const up = async (db) => {
   await db.execute(`
     CREATE TABLE IF NOT EXISTS chat_messages (
-<<<<<<< Updated upstream
-      id INT AUTO_INCREMENT PRIMARY KEY,
-
-      user_id VARCHAR(7) NOT NULL,
-
-      sender ENUM('user', 'ai') NOT NULL,
-=======
       id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 
       user_id VARCHAR(7) NOT NULL,
@@ -16,29 +9,10 @@ export const up = async (db) => {
         'user',
         'ai'
       ) NOT NULL,
->>>>>>> Stashed changes
 
       message_type ENUM(
         'chat',
         'overview',
-<<<<<<< Updated upstream
-        'exercise',
-        'feedback'
-      ) NOT NULL DEFAULT 'chat',
-
-      message TEXT NOT NULL,
-
-      createdAt TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-      updatedAt TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
-        ON UPDATE CURRENT_TIMESTAMP,
-
-      CONSTRAINT fk_chat_messages_user
-      FOREIGN KEY (user_id)
-      REFERENCES users(id)
-      ON DELETE CASCADE
-      ON UPDATE CASCADE
-    )
-=======
         'exercise'
       ) DEFAULT 'text',
 
@@ -55,7 +29,6 @@ export const up = async (db) => {
         ON UPDATE CASCADE
 
     ) ENGINE=InnoDB;
->>>>>>> Stashed changes
   `);
 };
 
