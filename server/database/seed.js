27472d6e86cf4eecf22edbcdeca8b93812be0b7c
kaisarnara <153050162+kaisarnara@users.noreply.github.com>
2026-06-02 14:14:45 +0700

    update_seed

--- server/database/seed.js
+++ server/database/seed.js
@@ -6,6 +6,8 @@ import usersSeed from "./seeds/003_users.seed.js";
 import { seedQuestions } from "./seeds/004_pretest_questions.seed.js";
 import { seedOptions } from "./seeds/005_pretest_options.seed.js";
 import { seedMaterialChunks } from "./seeds/006_material_chunks.seed.js";
+import seedQuizQuestions from "./seeds/007_quiz_questions.seed.js"; 
+import seedQuizOptions from "./seeds/008_quiz_options.seed.js";     
 
 const runSeeds = async () => {
   try {
@@ -22,6 +24,13 @@ const runSeeds = async () => {
     console.log("Options seeded");
     await seedMaterialChunks(db);
     console.log("Material chunks seeded");
+    
+    await seedQuizQuestions(db);
+    console.log("Quiz questions seeded");
+    
+    await seedQuizOptions(db);
+    console.log("Quiz options seeded");
+    
     console.log("Seeder selesai");
 
     process.exit(0);
